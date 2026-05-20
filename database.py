66543cd33f73d3cd5a9ac4fb6365a7608ee115a1
rishi24july <rishigolugupta@gmail.com>
2026-05-20 13:55:03 +0530

    fix pg8000 ssl issue

--- database.py
+++ database.py
@@ -4,14 +4,19 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 db_url = os.environ.get("DATABASE_URL", "sqlite:///./tripsplit.db")
 
-# pg8000 driver use karo PostgreSQL ke liye
-if "postgresql" in db_url and "pg8000" not in db_url:
+if "postgresql" in db_url:
+    # pg8000 use karo aur sslmode URL se hatao
     db_url = db_url.replace("postgresql://", "postgresql+pg8000://")
-
-engine = create_engine(
-    db_url,
-    connect_args={"check_same_thread": False} if "sqlite" in db_url else {}
-)
+    db_url = db_url.replace("?sslmode=require", "").replace("&sslmode=require", "")
+    engine = create_engine(
+        db_url,
+        connect_args={"ssl_context": True}
+    )
+else:
+    engine = create_engine(
+        db_url,
+        connect_args={"check_same_thread": False}
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
